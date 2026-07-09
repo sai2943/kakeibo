edccabffaa2ae8c0bf5000e702d9a3175c674301
sai2943 <e020040630@gmail.com>
2026-07-09 11:51:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-/* 収支台帳 service worker v36
+/* 収支台帳 service worker v37
    方針: ページ本体はネットワーク優先(更新が即反映)、失敗時キャッシュ(オフライン動作)。
    アイコン等の静的物はキャッシュ優先。 */
-const CACHE = "kakeibo-v36";
+const CACHE = "kakeibo-v37";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
